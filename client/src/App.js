020315e8be318e22837914b36cc707a803f6f16c
yvettehenderson <yvette.henderson@gmail.com>
2021-04-10 15:06:37 -0700

    google sign in button

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,49 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+class App extends React.Component {
+
+  insertGapiScript() {
+    const script = document.createElement('script')
+    script.src = 'https://apis.google.com/js/api.js'
+    script.onload = () => {
+      this.initializeGoogleSignIn()
+    }
+    document.body.appendChild(script)
+  }
+
+  initializeGoogleSignIn() {
+    window.gapi.load('auth2', () => {
+      window.gapi.auth2.init({
+        client_id: '135452617126-oo0lohdfakjgm8sdjtbdk02doojua4t2.apps.googleusercontent.com'
+      })
+      console.log('Api inited')
+
+      window.gapi.load('signin2', () => {
+        const params = {
+          onsuccess: () => {
+            console.log('User has finished signing in!')
+          }
+        }
+        window.gapi.signin2.render('loginButton', params)
+      })
+    })
+  }
+
+  componentDidMount() {
+    console.log('Loading')
+
+    this.insertGapiScript();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Moody Times</h1>
+        <a id="loginButton">Sign in with Google</a>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+    );
+  }
 }
-
 
 export default App;
