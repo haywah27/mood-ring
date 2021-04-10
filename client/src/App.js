0af27c8e2e88b21df8e75d842bd94bb9127da436
haywah27 <hayleywahlroos15@gmail.com>
2021-04-10 11:24:02 -0700

    html live links to blank pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [moods, setMoods] = useState([]);
+  const [reDownload, triggerReDownload] = useState("");
   return (
     <Router>
       <div>
-        <Link to="/moods"> All Notes</Link>
+        <Link to="/"> Home</Link>
+        <br />
+        <Link to="/moods"> All Moods</Link>
         <br />
         <Link to="/new-mood"> Check Mood</Link>
+        <br />
+        <Link to="/login"> Login</Link>
+        <br />
+        <Link to="/sign-up"> Sign Up</Link>
       </div>
+      <Switch>
+        <Route path="/moods">
+            hello all moods page
+        </Route>
+        <Route path="/new-mood">
+            hello new-mood page
+        </Route>
+        <Route path="/login">
+            hello login page
+        </Route>
+        <Route path="/sign-up">
+            hello sign-up page
+        </Route>
+      </Switch>
     </Router>
   );
 }
