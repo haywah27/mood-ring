c8990429310f9644995d4df427d5e5edeb0d27f9
haywah27 <hayleywahlroos15@gmail.com>
2021-04-10 11:13:38 -0700

    adding routes for html pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Link to="/moods"> All Notes</Link>
+        <br />
+        <Link to="/new-mood"> Check Mood</Link>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
