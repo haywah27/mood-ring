3c84585a6c5210d45657a17fd4bbfebb34b3c4d5
haywah27 <hayleywahlroos15@gmail.com>
2021-04-12 19:30:44 -0700

    created pages folder, initial routes on app file set up

--- client/src/App.js
+++ client/src/App.js
@@ -9,34 +9,44 @@ function App() {
   const [reDownload, triggerReDownload] = useState("");
   return (
     <Router>
-      <div>
-        <Link to="/"> Home</Link>
-        <br />
-        <Link to="/moods"> All Moods</Link>
-        <br />
-        <Link to="/new-mood"> Check Mood</Link>
-        <br />
-        <Link to="/login"> Login</Link>
-        <br />
-        <Link to="/sign-up"> Sign Up</Link>
-      </div>
+      <Navigation />
       <Switch>
-        <Route path="/moods">
-            hello all moods page
-        </Route>
-        <Route path="/new-mood">
-            hello new-mood page
-        </Route>
-        <Route path="/login">
-            hello login page
-        </Route>
-        <Route path="/sign-up">
-            hello sign-up page
-        </Route>
+        <Route path="/" exact component={() => <Home />} />
+        <Route path="/moods" exact component={() => <CaptureMood />} />
+        <Route path="/new-mood" exact component={() => <NewMood />} />
+        <Route path="/login" exact component={() => <Login />} />
+        <Route path="/sign-up" exact component={() => <SignUp />} />
       </Switch>
     </Router>
+
+    // <Router>
+    //   <div>
+    //     <Link to="/"> Home</Link>
+    //     <br />
+    //     <Link to="/moods"> All Moods</Link>
+    //     <br />
+    //     <Link to="/new-mood"> Check Mood</Link>
+    //     <br />
+    //     <Link to="/login"> Login</Link>
+    //     <br />
+    //     <Link to="/sign-up"> Sign Up</Link>
+    //   </div>
+    //   <Switch>
+    //     <Route path="/moods">
+    //         hello all moods page
+    //     </Route>
+    //     <Route path="/new-mood">
+    //         hello new-mood page
+    //     </Route>
+    //     <Route path="/login">
+    //         hello login page
+    //     </Route>
+    //     <Route path="/sign-up">
+    //         hello sign-up page
+    //     </Route>
+    //   </Switch>
+    // </Router>
   );
 }
-
 
 export default App;
