6785a0c953487b51e01c2279c4dd51089f53f58a
yvettehenderson <yvette.henderson@gmail.com>
2021-04-14 12:49:19 -0700

    changed code  for login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,37 +1,57 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
-import { GoogleLogin } from 'react-google-login';
-import {refreshTokenSetup} from '../utils/freshToken'
+// import './App.css';
 
-const clientId =
-  '135452617126-oo0lohdfakjgm8sdjtbdk02doojua4t2.apps.googleusercontent.com';
 
-function Login() {
-  const onSuccess = (res) => {
-    console.log('Login Success: currentUser:', res.profileObj);
 
-    refreshTokenSetup(res);
+export class Login extends React.Component {
+  
 
-  };
+responseGoogle=(response) =>{
+  console.log(response);
+  console.log(response.profileObj);
+}
 
-  const onFailure = (res) => {
-    console.log('Login failed: res:', res);
 
-  };
+  insertGapiScript() {
+    const script = document.createElement('script')
+    script.src = 'https://apis.google.com/js/api.js'
+    script.onload = () => {
+      this.initializeGoogleSignIn()
+    }
+    document.body.appendChild(script)
+  }
 
-  return (
-    <div>
-      <GoogleLogin
-        clientId={clientId}
-        buttonText="Login"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={'single_host_origin'}
-        style={{ marginTop: '100px' }}
-        isSignedIn={true}
-      />
-    </div>
-  );
+  initializeGoogleSignIn() {
+    window.gapi.load('auth2', () => {
+      window.gapi.auth2.init({
+        client_id: '135452617126-oo0lohdfakjgm8sdjtbdk02doojua4t2.apps.googleusercontent.com'
+      })
+      console.log('Api inited')
+
+      window.gapi.load('signin2', () => {
+        const params = {
+          onsuccess: () => {
+            console.log('User has finished signing in!')
+          }
+        }
+        window.gapi.signin2.render('loginButton', params)
+      })
+    })
+  }
+
+  componentDidMount() {
+    console.log('Loading')
+
+    this.insertGapiScript();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <a id="loginButton">Sign in with Google</a>
+      </div>
+    );
+  }
 }
 
 export default Login;
