c45f7c1469f7d62500fa880e8023d26e1bf679ca
haywah27 <hayleywahlroos15@gmail.com>
2021-04-13 19:56:59 -0700

    home page updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Home.css";
+import { Jumbotron, Container } from "react-bootstrap";
 
 function Home() {
-  return <div>hello home page</div>;
+  return (
+    <div>
+      hello home page
+      <Container >
+        <Jumbotron className="dimension text-center" fluid>
+          <Container >
+            <h1 className="title">Welcome to Moody Times!</h1>
+            <h5 className="body">
+              We work to determine your current mood and provide content to match..Blahblahblah. WIP.
+            </h5>
+          </Container>
+        </Jumbotron>
+      </Container>
+    </div>
+  );
 }
 
 export default Home;
