e8aa8ae808a771e22b39806fe91a5c98fabf9b07
haywah27 <hayleywahlroos15@gmail.com>
2021-04-15 11:20:01 -0700

    formatting login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,46 +1,42 @@
-import React from 'react';
-// import './App.css';
-
-
+import React from "react";
+// import "./App.css";
 
 export class Login extends React.Component {
-  
-
-responseGoogle=(response) =>{
-  console.log("google res", response);
-  console.log("google portfolioOBJ",response.profileObj);
-}
-
+  responseGoogle = (response) => {
+    console.log("google res", response);
+    console.log("google portfolioOBJ", response.profileObj);
+  };
 
   insertGapiScript() {
-    const script = document.createElement('script')
-    script.src = 'https://apis.google.com/js/api.js'
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/api.js";
     script.onload = () => {
-      this.initializeGoogleSignIn()
-    }
-    document.body.appendChild(script)
+      this.initializeGoogleSignIn();
+    };
+    document.body.appendChild(script);
   }
 
   initializeGoogleSignIn() {
-    window.gapi.load('auth2', () => {
+    window.gapi.load("auth2", () => {
       window.gapi.auth2.init({
-        client_id: '135452617126-oo0lohdfakjgm8sdjtbdk02doojua4t2.apps.googleusercontent.com'
-      })
-      console.log('Api inited')
+        client_id:
+          "135452617126-oo0lohdfakjgm8sdjtbdk02doojua4t2.apps.googleusercontent.com",
+      });
+      console.log("Api inited");
 
-      window.gapi.load('signin2', () => {
+      window.gapi.load("signin2", () => {
         const params = {
           onsuccess: () => {
-            console.log('User has finished signing in!')
-          }
-        }
-        window.gapi.signin2.render('loginButton', params)
-      })
-    })
+            console.log("User has finished signing in!");
+          },
+        };
+        window.gapi.signin2.render("loginButton", params);
+      });
+    });
   }
 
   componentDidMount() {
-    console.log('Loading')
+    console.log("Loading");
 
     this.insertGapiScript();
   }
