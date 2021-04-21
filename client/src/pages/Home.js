b68b3cd33784a997edeaf7bea5ce3235b4d97f25
haywah27 <hayleywahlroos15@gmail.com>
2021-04-20 19:40:03 -0700

    home page body updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,12 +10,15 @@ function Home() {
           <Container>
             <h1 className="title">Welcome to Mood Ring!</h1>
             <h5 className="body">
-              We work to determine your current mood and provide content to
-              match..Blahblahblah. WIP.
+              This is an application that uses facial recognition to capture your current emotion and deliver content that serves as a type of "remedy" or "boost" to your captured emotion. Users who login will be able to see a history of previous moods or you can decide to just play with the app without having to create an account.
             </h5>
             <br />
+            <Button href="/login" className="getStartedButton" size="huge">
+              Login
+            </Button>
+            <span className="subtitle"> OR </span>
             <Button href="/new-mood" className="getStartedButton" size="huge">
-              Get Started
+              Check Mood
             </Button>
           </Container>
         </Jumbotron>
