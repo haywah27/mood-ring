41e6f628fed740a7606da19b260d38ecda14df6c
haywah27 <hayleywahlroos15@gmail.com>
2021-04-20 20:00:10 -0700

    home page content updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,9 +9,13 @@ function Home() {
         <Jumbotron className="dimension text-center" fluid>
           <Container>
             <h1 className="title">Welcome to Mood Ring!</h1>
+            <hr />
             <h5 className="body">
-              This is an application that uses facial recognition to capture your current emotion and deliver content that serves as a type of "remedy" or "boost" to your captured emotion. Users who login will be able to see a history of previous moods or you can decide to just play with the app without having to create an account.
+              Explore our facial recognition technology to capture your current emotion and we'll provide content that serves as a type of "remedy" or "boost" to your current emotion. 
+              <hr />
+              Users who login will be able to see a history of previous moods or you can decide to just play with the app without having to create an account.
             </h5>
+            <br />
             <br />
             <Button href="/login" className="getStartedButton" size="huge">
               Login
