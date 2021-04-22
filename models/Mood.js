f34ed86f44d3f918dc3ce64a045a477f29954750
amatt <amattson84@gmail.com>
2021-04-22 16:31:12 -0700

    Accepting changes from merge.

--- models/Mood.js
+++ models/Mood.js
@@ -2,14 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const moodSchema = new Schema({
-<<<<<<< HEAD
-  id: {  type: String, required: true },
-  expression: { type: String, required: "Sorry.Can't save an empty mood." },
-=======
     user: { type: String },
     googleID: { type: String },
     expressions: { type: String },
->>>>>>> f5c1cfce4ab3d9d7dc1eca896f245c327680b307
 });
 
 const Mood = mongoose.model("Mood", moodSchema);
