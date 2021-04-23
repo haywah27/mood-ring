cc3f9b038631fbb25b6e4e526c9c96735e5b8861
amatt <amattson84@gmail.com>
2021-04-22 21:03:15 -0700

    Test files for profile component.

---
+++ client/src/components/showProfile/User.js
@@ -0,0 +1,105 @@
+
+
+
+class ShowProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      expression: '',
+      googleId: '',
+    };
+  }
+  componentDidMount() {
+    document.getElementByClass('new-mood').className = '';
+    document.getElementByClass('login').className = '';
+    document.getElementByClass('home').className = '';
+    document.getElementByClass('user').className = 'active';
+    this.getProfile();
+  }
+
+  getProfile() {
+    var self = this;
+    axios.post('/getProfile', {
+    })
+    .then(function (response) {
+        self.setState({name:response.data.name});
+        self.setState({expression:response.data.expressionl});
+        
+         
+    })
+    .catch(function (error) {
+        console.log('error is ',error);
+    });
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Jumbotron className='userDisplay' fluid>
+            <Container>
+              <div className='col-md-5'>
+                <h1 className='userTitle'> My Profile </h1>
+
+                <div className='userBody'>
+                  <input
+                    value={this.state.name}
+                    type='text'
+                    onChange={this.handleNameChange}
+                    className='form-control'
+                    placeholder='Name'
+                    required
+                  />
+                  <input
+                    value={this.state.expression}
+                    type='expression'
+                    className='form-control'
+                    placeholder='expression'
+                    required
+                  />
+                </div>
+
+                <input
+                  value={this.state.expression}
+                  type='expression'
+                  className='form-control'
+                  placeholder='expression'
+                  required
+                />
+
+                <button
+                  type='button'
+                  onClick={this.updateProfile}
+                  id='submit'
+                  name='submit'
+                  className='btn btn-primary pull-right'
+                >
+                  Update
+                </button>
+              </div>
+            </Container>
+          </Jumbotron>
+        </Container>
+      </div>
+    );
+  }
+}
+
+function getUserInfo(username, callback){
+    MongoClient.connect(url, function(err, db){
+         
+        db.collection('user').findOne( { email : username 
+        },function(err, result){
+            if(result==null){
+                callback(false)
+            }
+            else{
+                callback(result);
+            }
+        });
+         
+    });
+};
