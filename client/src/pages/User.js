71ac14e26ab61d455eb09924e6e26baf9e238157
amatt <amattson84@gmail.com>
2021-04-22 21:45:12 -0700

    Update code so that user information is rendered.

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,52 +1,91 @@
 import React from 'react';
 import './User.css';
+import { Jumbotron, Container, Button } from 'react-bootstrap';
+import axios from "axios";
 
-export class User extends React.Component {
+class User extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      person: {
-        name: 'Wolverine, X-Men',
-        biography:
-          'Founder, X-style Hair salon & Spa. had a hard day fighting invading mutants ? Let our hair designers whisk you away with killer style! Graduate of Metal Nail Salons. Literally.',
-      },
-      image:
-        'https://i.pinimg.com/originals/f2/54/82/f25482a6daf9c8f73967dcbbdfe67910.jpg',
-      quote: {
-        content: 'He who rules the remote, rules the world',
-        source: 'Garfield the Cat',
-      },
+      name: '',
+      expression: '',
+      googleId: '',
     };
   }
+  componentDidMount() {
+    document.getElementsByClassName('new-mood').className = '';
+    document.getElementsByClassName('login').className = '';
+    document.getElementsByClassName('home').className = '';
+    document.getElementsByClassName('user').className = 'active';
+    this.getProfile();
+  }
+
+  getProfile() {
+    var user = this;
+    axios
+      .post('/getProfile', {})
+      .then(function (response) {
+        user.setState({ name: response.data.name });
+        user.setState({ expression: response.data.expression });
+      })
+      .catch(function (error) {
+        console.log('error is ', error);
+      });
+  }
+
   render() {
     return (
-      <div className='User'>
-        <Image src={this.state.image} />
-        <Profile person={this.state.person} quote={this.state.quote} />
+      <div>
+        <Container>
+          <Jumbotron className='userDisplay' fluid>
+            <Container>
+              <div className='col-md-5'>
+                <h1 className='userTitle'> My Profile </h1>
+
+                <div className='userBody'>
+                  <input
+                    value={this.state.name}
+                    type='text'
+                    placeholder='Name'
+                    required
+                  />
+                  <input
+                    value={this.state.expression}
+                    type='expression'
+                    placeholder='expression'
+                    required
+                  />
+                </div>
+
+                {/* <button
+                  type='button'
+                  onClick={this.updateProfile}
+                  id='submit'
+                  name='submit'
+                  className='btn btn-primary pull-right'
+                >
+                  Update
+                </button> */}
+              </div>
+            </Container>
+          </Jumbotron>
+        </Container>
       </div>
     );
   }
 }
 
-function Image(props) {
-  return (
-    <div
-      className='Image'
-      style={{ backgroundImage: 'url(' + props.src + ')' }}
-    ></div>
-  );
-}
-function Profile(props) {
-  return (
-    <div className='Profile'>
-      <h1 className='Name'>{props.person.name}</h1>
-      <p className='Bio'>{props.person.biography}</p>
-      <div className='Quote'>
-        <blockquote>&ldquo; {props.quote.content} &rdquo;</blockquote>
-        <div className='byline'>&mdash; {props.quote.source}</div>
-      </div>
-    </div>
-  );
-}
+// function getUserInfo(username, callback) {
+//   MongoClient.connect(url, function (err, db) {
+//     db.collection('user').findOne({ name: "Name" }, function (err, result) {
+//       if (result == null) {
+//         callback(false);
+//       } else {
+//         callback(result);
+//       }
+//     });
+//   });
+
+// }
 
 export default User;
