96f4ad8c9411298c3c7d4f1276805c4c7cfef4e0
amatt <amattson84@gmail.com>
2021-04-23 20:35:03 -0700

    Refactor mongoose.connect route to be hosted by mongoDBAtlas instead of localhost.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const morgan = require("morgan")
-const path = require("path");
+
+
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 const app = express();
-const MoodsApiRoute = require('./routes/MoodsApi');
+const PORT = process.env.PORT || 3001;
+const path = require("path");
+
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,8 +20,17 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+// Add routes, both API and view
+app.use(routes);
+
 // Define API routes here
+
+app.use('/api', MoodsApiRoute);
+
+
 app.use(MoodsApiRoute);
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
@@ -25,8 +38,21 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/moods");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://aimee_admin:AimeePW@reactmoodring.gzg5j.mongodb.net/Mood-ring", {useNewUrlParser: true}, {useUnifiedTechnology: true}) ;
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://aimee_admin:moodRingApp@reactmoodring.gzg5j.mongodb.net/moods", {useNewUrlParser: true}, {useUnifiedTechnology: true}) ;
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://aimee_admin:moodRingApp@reactmoodring.gzg5j.mongodb.net/moods", {useNewUrlParser: true}, {useUnifiedTechnology: true}) ;
+
+
+
+// mongodb+srv://aimee_admin:<password>@reactmoodring.gzg5j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
+
+Refactor mongoose.connect route to be hosted by mongoDBAtlas instead of localhost. 
