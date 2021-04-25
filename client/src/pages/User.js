51d0e974470ba67ab1095261f7d548cf7290ece4
haywah27 <hayleywahlroos15@gmail.com>
2021-04-25 15:20:47 -0700

    mapping mood array from local storage

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -5,44 +5,47 @@ import { Jumbotron, Container, ListGroup } from "react-bootstrap";
 import API from "../utils/API";
 
 // class User extends Component{
+// window.location.reload();
+
+
+
 function User() {
   const localUser = JSON.parse(localStorage.getItem("Profile"));
-  
+  let yourMom;
+  let moodList;
+
 
   function getMoods() {
-    let expressions;
-    API.findProfileUser(localUser[0].googleId).then(function (response) {
-      console.log("this is new response", response);
-    });
-  }
-
-  function userLoggedIn() {
     if (localStorage.getItem("Profile") !== null) {
-      return (localUser[0].name);
+      yourMom = localUser[0].expressions;
+
+      moodList = yourMom.map((mood) => <ListGroup.Item>{mood}</ListGroup.Item>);
+
+      console.log("mom", yourMom);
+      return (
+        <>
+          <h1 className="title">Hello, {localUser[0].name}!</h1>
+          <hr />
+          <h1 className="subtitle">Here's Your Mood History</h1>
+          <ListGroup variant="flush" className="expressionHistory">
+            {moodList.slice(Math.max(moodList.length - 5, 0))}
+          </ListGroup>
+          <br />
+          <br />
+        </>
+      );
     } else {
-      return ("User Not Logged In")
+      return <h1 className="subtitle">Please Login To See Mood History</h1>;
     }
   }
+
 
   return (
     <>
       <Navigation />
       <Container>
         <Jumbotron className="dimension text-center" fluid>
-          <Container>
-            <h1 className="title">{userLoggedIn()}</h1>
-            <hr />
-            <h1 className="subtitle">Here's Your Mood History</h1>
-            <ListGroup variant="flush" className="expressionHistory">
-              <ListGroup.Item>Expression :</ListGroup.Item>
-              <ListGroup.Item>Expression</ListGroup.Item>
-              <ListGroup.Item>Expression</ListGroup.Item>
-              <ListGroup.Item>Expression</ListGroup.Item>
-              <ListGroup.Item>Expression</ListGroup.Item>
-            </ListGroup>
-            <br />
-            <br />
-          </Container>
+          <Container>{getMoods()}</Container>
         </Jumbotron>
       </Container>
     </>
